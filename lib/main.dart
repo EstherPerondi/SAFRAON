ae690098c23264c7f773954434e448ed52de95c0
Yasmim Lima <limaceremyasmim@gmail.com>
2026-08-04 09:56:04 -0300

    Talhao

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:safraon/aplicacoes.dart';
 import 'package:safraon/clima.dart';
 import 'package:safraon/colheita.dart';
 import 'package:safraon/precipitacao.dart';
+import 'package:safraon/telas/cadastro.dart';
 import 'package:safraon/telas/fazendas.dart';
 import 'package:safraon/telas/login.dart';
 import 'package:safraon/telas/manejo.dart';
@@ -42,7 +43,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ClimaPage(),
+      home: TalhaoPage(),
     );
   }
 }
